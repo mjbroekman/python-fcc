5fcec6be4bef7ec85215ecb212f007cf885bc215
Maarten Broekman <maarten@broekman.us>
2022-10-08 19:52:28 -0400

    cleaned up and consolidated

--- madlibs/madlib.py
+++ madlibs/madlib.py
@@ -7,10 +7,9 @@ This picks a random madlib from the defined set in random_madlibs and cycles thr
 import random
 import random_madlibs
 
-madlib = random_madlibs.madlibs[random.choice(list(random_madlibs.madlibs.keys()))]
+ml_name = random.choice(list(random_madlibs.madlibs.keys()))
+madlib = random_madlibs.madlibs[ ml_name ]
 for key in madlib['vars']:
     madlib['text'] = madlib['text'].replace(f'---{key}---',input(madlib['vars'][key]))
 
-print()
-print(list(random_madlibs.madlibs.keys()))
-print(madlib['text'])
+print( "\n{}\n{}\n".format( ml_name, madlib['text'] ) )
