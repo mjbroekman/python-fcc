861a9dbdaa4fdb86392b14c1b701298412e4494b
Maarten Broekman <maarten@broekman.us>
2022-10-15 14:52:43 -0400

    converted to have both user-guess and computer-guess in the same file.

--- guessing_game/guess.py
+++ guessing_game/guess.py
@@ -3,6 +3,11 @@ Guess
 -----
 A guessing game of variable difficulty.
 
+Author: Maarten Broekman
+
+Inspired by: https://www.freecodecamp.org/news/python-projects-for-beginners/#guess-the-number-game-python-project-computer-
+Expanded on 
+
 Options:
   --low: the lowest number. Default is 0
   --high: the highest number. Default is 10
@@ -11,49 +16,84 @@ Options:
 '''
 
 import argparse
+from operator import contains
 import sys
 import random
 
 from fractions import Fraction
 
+def user_game(low, high, guess):
+    target_number = random.randint(low,high)
+    user_guess = low - 1
+    guess_num = 1
+    print(f"Guess a number between {low} and {high}. You have {guess} or fewer guesses to do this.")
+    while guess_num <= guess:
+        user_guess = int(input(f"Guess a number ({guess_num}): "))
+        if low <= user_guess <= high:
+            guess_num += 1
+            if user_guess < target_number:
+                print("Good guess, but you're too low. Try again.\n")
+            elif user_guess > target_number:
+                print("Good guess, but you're too high. Try again.\n")
+            else:
+                print("Amazing! You guessed the number!\n")
+                break
+        else:
+            print("Come on. At least guess a number in the range...")
+    else: # No break
+        print(f"\nThat was tough. You ran out of guesses. The number I was thinking of was {target_number}.")
+
+def comp_game(low, high, guess):
+    print(f"I'm going to guess a number between {low} and {high}. I have {guess} or fewer guesses to get it.")
+    guess_num = 1
+    valid_feedback = [ 'H', 'L', 'C' ]
+    while guess_num <= guess:
+        comp_guess = low + int( (high - low) / 2 )
+        feedback = ''
+        feedback = input(f"I'm going to guess: {comp_guess}. Is that high (H), low (L), or correct (C)? ")
+        while feedback.capitalize() not in valid_feedback:
+            print("I'm sorry. I didn't understand your feedback.")
+            feedback = input(f"I'm going to guess: {comp_guess}. Is that high (H), low (L), or correct (C)? ")
+
+        guess_num += 1
+        if feedback.capitalize() == 'H':
+            high = comp_guess - 1
+            print(f"Hmmm. I was too high with {comp_guess}. Let me try again. This time between {low} and {high}\n")
+        if feedback.capitalize() == 'L':
+            low = comp_guess + 1
+            print(f"Hmmm. I was too low with {comp_guess}. Let me try again. This time between {low} and {high}\n")
+        if feedback.capitalize() == 'C':
+            print("Amazing! I was able to guess your number! Computers are really smart, aren't we?\n")
+            break
+    else: # No break
+        print("\nWow! That was tough. I ran out of guesses.")
+
 parser = argparse.ArgumentParser(description="A Guessing Game of variable difficulty.")
 parser.add_argument("--low","-l",action="store",type=int,help="The lowest number of the range to guess from.",default=0)
 parser.add_argument("--high","-H",action="store",type=int,help="The highest number of the range to guess from.",default=10)
 parser.add_argument("--guess","-g",action="store",type=int,help="The number of guesses available.",default=3)
+parser.add_argument("--comp","-c",action="store_true",help="Make the project guess the number.")
 args = parser.parse_args()
 
+# Swap the high and low of the range to avoid silliness
 if args.low > args.high:
     args.low, args.high = args.high, args.low
 
+# Bail out if the high and low are the same
 if args.low == args.high:
     print("That's not much a challenge... be serious")
     sys.exit(2)
 
+# Calculate the width and optimal guess count 
 width = args.high - args.low
 bin_tree = 2 ** args.guess
 
+# If a binary tree is GUARANTEED to find the right answer, then it's too easy
 if Fraction(width,bin_tree) <= 1:
     print("That's not very challenging. Maybe try a wider range of numbers or fewer guesses.")
     sys.exit(2)
 
-target_number = random.randint(args.low,args.high)
-user_guess = args.low - 1
-guess_num = 1
-print(f"Guess a number between {args.low} and {args.high}. You have {args.guess} or fewer guesses to do this.")
-while guess_num <= args.guess:
-    user_guess = int(input(f"Guess a number ({guess_num}): "))
-    if args.low <= user_guess <= args.high:
-        if user_guess < target_number:
-            print("Good guess, but you're too low. Try again.")
-            guess_num += 1
-        elif user_guess > target_number:
-            print("Good guess, but you're too high. Try again.")
-            guess_num += 1
-        else:
-            print("Amazing! You guessed the number!")
-            break
-    else:
-        print("Come on. At least guess a number in the range...")
-else: # No break
-    print(f"That was tough. You ran out of guesses. The number I was thinking of was {target_number}.")
-
+if args.comp:
+    comp_game(args.low,args.high,args.guess)
+else:
+    user_game(args.low,args.high,args.guess)
